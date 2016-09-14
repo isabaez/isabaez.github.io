9fd5bc61706bb772b4296145d88da91f31dd7b67
Uriel Baez <isabaez@seas.upenn.edu>
2016-09-14 18:35:09 -0400

    toggling containers

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,15 @@ $(document).ready(function() {
 	console.log("Ready!")
 
 	$('#home').click(function() {
-	console.log("You clicked on home")
-	$('#home-container').toggle()
+		console.log("You clicked on home")
+		$('#about-container').toggle(false)
+		$('#home-container').toggle(true)
 	})
 
 	$('#about').click(function() {
 		console.log("You clicked on about")
+		$('#home-container').toggle(false)
+		$('#about-container').toggle(true)
 	})
 
 	$('#resume').click(function() {
