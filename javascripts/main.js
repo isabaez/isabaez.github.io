cf92936e73d2a12180cdd15f1347eaff2f5a997d
Uriel Baez <isabaez@seas.upenn.edu>
2016-09-14 17:56:23 -0400

    jQuery for toggling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 	console.log("Ready!")
 })
+
+$('#home').click(function() {
+	console.log("You clicked on home")
+	$('#home-container').toggle()
+})
+
+$('#about').click(function() {
+	console.log("You clicked on about")
+})
+
+$('#resume').click(function() {
+	console.log("You clicked on resume")
+})
