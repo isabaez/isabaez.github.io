9ddf05a4fea28cf14b2df4b7e03071bca046fac2
Uriel Baez <isabaez@seas.upenn.edu>
2016-09-14 18:49:18 -0400

    added connect link functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,18 +2,20 @@ $(document).ready(function() {
 	console.log("Ready!")
 
 	$('#home').click(function() {
-		console.log("You clicked on home")
 		$('#about-container').toggle(false)
+		$('#connect-container').toggle(false)
 		$('#home-container').toggle(true)
 	})
 
 	$('#about').click(function() {
-		console.log("You clicked on about")
 		$('#home-container').toggle(false)
+		$('#connect-container').toggle(false)
 		$('#about-container').toggle(true)
 	})
 
-	$('#resume').click(function() {
-		console.log("You clicked on resume")
+	$('#connect').click(function() {
+		$('#home-container').toggle(false)
+		$('#about-container').toggle(false)
+		$('#connect-container').toggle(true)
 	})
 })
