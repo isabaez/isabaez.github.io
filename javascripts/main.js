360e6eeaec5d94dfe84973311915411460cf6eb6
Uriel Baez <isabaez@seas.upenn.edu>
2016-09-15 22:29:16 -0400

    toggling stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
 
 	$('#about').click(function() {
+		$('#intro').toggle(false);
+		$('#intro-desc').toggle(false)
 		$('#container-left').fadeIn("slow")
 	})
 
 	$('#projects').click(function() {
+		$('#intro').toggle(false);
+		$('#intro-desc').toggle(false)
 		$('#container-mid').fadeIn("slow")
 	})
 
 	$('#connect').click(function() {
+		$('#intro').toggle(false);
+		$('#intro-desc').toggle(false)
 		$('#container-right').fadeIn("slow")
 	})
 
