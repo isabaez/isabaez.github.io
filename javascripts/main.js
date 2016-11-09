e92582821b8621a9519f49d1b0076d474e10d8bd
Uriel Baez <isabaez@seas.upenn.edu>
2016-11-09 15:41:30 -0500

    zoom styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,12 +21,11 @@ $(document).ready(function() {
 		$(".enlarge").fadeToggle(500)
 		$("#spotlight").attr('src', enlarge_src)
 		if (enlarge_src === "images/evie1.png" || enlarge_src === "images/evie2.png") {
-			debugger;
-			$("#spotlight").css({'height': '80%'});
+			$("#spotlight").css('height', '80%');
 			$("#spotlight").removeProp('width');
 
 		} else {
-			$("#spotlight").css({'width': '80%'});
+			$("#spotlight").css('width', '80%');
 			$("#spotlight").removeProp('height');
 		}
 	})
